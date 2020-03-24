a7e0f3fe4bb22986aae54a1e1e4260ebd6a13a3f
Pedro Neto <pedromneto97@gmail.com>
2020-03-24 12:38:08 -0300

    Add: Adiciona relation pra interface

--- src/Interfaces/ManipulationManagerInterface.php
+++ src/Interfaces/ManipulationManagerInterface.php
@@ -6,7 +6,7 @@ namespace Freelabois\LaravelQuickstart\Interfaces;
 
 interface ManipulationManagerInterface
 {
-    public function storeOrUpdate($values, int $id = null);
+    public function storeOrUpdate($values, int $id = null, array $relations = []);
 
     public function validate($values, int $id = null);
 
